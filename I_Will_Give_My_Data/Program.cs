b353f97e3cefa13af355242c38e3ed84864d56b8
ShemenV <103890522+ShemenV@users.noreply.github.com>
2023-12-02 22:05:54 +0500

    Update Program.cs

--- I_Will_Give_My_Data/Program.cs
+++ I_Will_Give_My_Data/Program.cs
@@ -1,20 +1,30 @@
-﻿using System.Net.Sockets;
-using System.Net;
+﻿using System.Management;
+using System.Net.Sockets;
 
-TcpListener server = new TcpListener(IPAddress.Any, порт);
-server.Start();
 
-while (true)
+string os = Environment.OSVersion.ToString();
+Console.WriteLine(os.ToString());// просто посмотреть для теста, потом эту строчку надо удалить 
+
+string driveInfo = string.Empty;
+foreach (DriveInfo drive in DriveInfo.GetDrives())
 {
-    TcpClient client = server.AcceptTcpClient(); // Принимаем подключение от клиента
-
-    // Читаем данные от клиента
-    StreamReader reader = new StreamReader(client.GetStream());
-    string data = reader.ReadToEnd();
-    client.Close();
-
-    // Сохраняем данные в текстовый файл
-    string fileName = $"данные_{DateTime.Now:yyyyMMddHHmmss}.txt";
-    File.WriteAllText(fileName, data);
+    if (drive.IsReady)
+    {
+        driveInfo += $"Диск {drive.Name}: Свободное место - {drive.AvailableFreeSpace}\n";
+        
+    }
 }
+Console.WriteLine(driveInfo); // просто посмотреть для теста, потом эту строчку надо удалить 
 
+
+string data = $"Операционная система: {os}nСвободное место на диске:n{driveInfo}"; // подготавливаем наши данные в переменную
+
+
+//TcpClient client = new TcpClient("IP_адрес_сервера", "порт_сервера"); // подключаемся к серверу и создаём клиента 
+
+//// Здесь данные должны отправляться на сервер 
+
+//StreamWriter writer = new StreamWriter(client.GetStream());
+//writer.WriteLine(data);
+//writer.Flush();
+//client.Close();
