3e6abe6495335bfc5389e138d14c9a2d685a2673
FireFeuer <aidar.bictimirov2017@yandex.ru>
2023-12-02 21:31:25 +0500

    version 0.1

--- I_Will_Give_My_Data/Program.cs
+++ I_Will_Give_My_Data/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Почему Git решил, что это Smalltalk?");
+﻿using System.Net.Sockets;
+using System.Net;
+
+TcpListener server = new TcpListener(IPAddress.Any, порт);
+server.Start();
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient(); // Принимаем подключение от клиента
+
+    // Читаем данные от клиента
+    StreamReader reader = new StreamReader(client.GetStream());
+    string data = reader.ReadToEnd();
+    client.Close();
+
+    // Сохраняем данные в текстовый файл
+    string fileName = $"данные_{DateTime.Now:yyyyMMddHHmmss}.txt";
+    File.WriteAllText(fileName, data);
+}
+
