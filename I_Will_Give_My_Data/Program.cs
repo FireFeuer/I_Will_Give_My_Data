b66c6a8e00f252f304139fbdf5e191425da1a83e
FireFeuer <aidar.bictimirov2017@yandex.ru>
2023-12-03 19:41:20 +0500

    version 0.2 Server connection, normal formatting, gigabytes not bytes, list of programs

--- I_Will_Give_My_Data/Program.cs
+++ I_Will_Give_My_Data/Program.cs
@@ -1,5 +1,21 @@
-﻿using System.Management;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
+using Microsoft.Win32;
+
+
+// Код на получение программ
+
+string displayName = "";
+RegistryKey key;
+
+key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+foreach (String keyName in key.GetSubKeyNames())
+{
+    RegistryKey subkey = key.OpenSubKey(keyName);
+    displayName += subkey.GetValue("DisplayName") as string + "\n";
+}
+
+Console.WriteLine(displayName);// просто посмотреть для теста, потом эту строчку надо удалить 
+
 
 
 string os = Environment.OSVersion.ToString();
@@ -10,21 +26,25 @@ foreach (DriveInfo drive in DriveInfo.GetDrives())
 {
     if (drive.IsReady)
     {
-        driveInfo += $"Диск {drive.Name}: Свободное место - {drive.AvailableFreeSpace}\n";
-        
+        double AvailableFreeSpace_Gb = Convert.ToDouble(((drive.AvailableFreeSpace / Convert.ToDouble(1024)) / Convert.ToDouble(1024)) / Convert.ToDouble(1024));
+        AvailableFreeSpace_Gb = Math.Round(AvailableFreeSpace_Gb, 2);
+        driveInfo += $"Диск {drive.Name}: Свободное место - {AvailableFreeSpace_Gb} Gb\n";
     }
 }
 Console.WriteLine(driveInfo); // просто посмотреть для теста, потом эту строчку надо удалить 
 
 
-string data = $"Операционная система: {os}nСвободное место на диске:n{driveInfo}"; // подготавливаем наши данные в переменную
+string data = $"Операционная система: {os}\nСвободное место на диске:\n{driveInfo}\n\nСписок программ:\n{displayName}"; // подготавливаем наши данные в переменную
 
 
-//TcpClient client = new TcpClient("IP_адрес_сервера", "порт_сервера"); // подключаемся к серверу и создаём клиента 
+TcpClient client = new TcpClient("26.194.255.228", 3333); // подключаемся к серверу и создаём клиента 
 
-//// Здесь данные должны отправляться на сервер 
+// Здесь данные должны отправляться на сервер 
 
-//StreamWriter writer = new StreamWriter(client.GetStream());
-//writer.WriteLine(data);
-//writer.Flush();
-//client.Close();
+StreamWriter writer = new StreamWriter(client.GetStream());
+writer.WriteLine(data);
+writer.Flush();
+client.Close();
+
+
+
