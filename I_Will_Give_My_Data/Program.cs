652d5bf7b07d9d4d3776f9c38452fab392387bd7
ShemenV <demonethdr@gmail.com>
2023-12-14 19:27:38 +0500

    version 1.1 раскидал на методы

--- I_Will_Give_My_Data/Program.cs
+++ I_Will_Give_My_Data/Program.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Sockets;
-using System.Reflection;
 using System.Text.Json;
 using System.Text.Unicode;
 using WUApiLib;
@@ -13,15 +12,6 @@ class Program
     public static async Task Main(string[] args)
     {
         // Код на получение информации о необходимости обновления Windows 
-
-        static bool IsWindowsUpdateNeeded()
-        {
-            UpdateSession updateSession = new UpdateSession();
-            IUpdateSearcher updateSearcher = updateSession.CreateUpdateSearcher();
-            ISearchResult searchResult = updateSearcher.Search("IsInstalled=0");
-
-            return searchResult.Updates.Count > 0;
-        }
 
         string isWindowsUpdateNeeded = "";
         if (IsWindowsUpdateNeeded() == true)
@@ -36,117 +26,40 @@ class Program
 
 
         // Код на получение программ
-
-        string displayName = "";
-        RegistryKey key;
-        int i = 0;
-        int b = 0;
-        key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
-        foreach (String keyName in key.GetSubKeyNames())
-        {
-            RegistryKey subkey = key.OpenSubKey(keyName);
-
-            if (subkey.GetValueNames().Contains("DisplayName"))
-                displayName += subkey.GetValue("DisplayName") as string + "\n";
-        }
-
+        string applicationsList = await GetApplicationsList();
+        string drivers = await GetDriversList();
         string os = Environment.OSVersion.ToString();
-
-        string driveInfo = string.Empty;
-        foreach (DriveInfo drive in DriveInfo.GetDrives())
-        {
-            if (drive.IsReady)
-            {
-                double AvailableFreeSpace_Gb = Convert.ToDouble(((drive.AvailableFreeSpace / Convert.ToDouble(1024)) / Convert.ToDouble(1024)) / Convert.ToDouble(1024));
-                AvailableFreeSpace_Gb = Math.Round(AvailableFreeSpace_Gb, 2);
-                driveInfo += $"Диск {drive.Name}: Свободное место - {AvailableFreeSpace_Gb} Gb\n";
-            }
-        }
-
-
         // Получение названия ПК
         string machineName = Environment.MachineName;
 
-        //string data = $"Операционная система: {os}\nСвободное место на диске:\n{driveInfo}\n\nСписок программ:\n{displayName}"; // подготавливаем наши данные в переменную (код-наследие)
-
         // Получаем события связанные с системой.
-
-        EventLog eventLogSystem = new EventLog("System");
-
+        string eventsSystem = await GetEventLog("System");
         // Получаем события связанные с приложениями.
-
-        EventLog eventLogApplication = new EventLog("Application");
-
-        // Создаем переменные для хранения нужных нам событий
-
-        string eventsSystem = "";
-
-        string eventsApplication = "";
-
-
-        foreach (EventLogEntry entry in eventLogSystem.Entries)
-        {
-            // Поучаем только ошибки и критические ошибки
-            if (entry.EntryType == EventLogEntryType.Error || entry.EntryType == EventLogEntryType.FailureAudit)
-            {
-                eventsSystem += entry.EventID.ToString() + "\n" + entry.Source + "\n" + entry.TimeGenerated.ToString() + "\n" + entry.Message + "\n\n";
-            }
-        }
-
-        foreach (EventLogEntry entry in eventLogApplication.Entries)
-        {
-            // Поучаем только ошибки и критические ошибки
-            if (entry.EntryType == EventLogEntryType.Error || entry.EntryType == EventLogEntryType.FailureAudit)
-            {
-                eventsApplication += entry.EventID.ToString() + "\n" + entry.Source + "\n" + entry.TimeGenerated.ToString() + "\n" + entry.Message + "\n\n";
-            }
-        }
-
-
+        string eventsApplication = await GetEventLog("Application");
 
 
         // код конфигурационного файла config.env
-
-        string path = Directory.GetCurrentDirectory();
-        string pathToEnvFile = Path.Combine(path, "config.env");
-        if (!File.Exists(pathToEnvFile))
-        {
-            await File.WriteAllTextAsync(pathToEnvFile, $"IP=\r\nPORT=3333");
-        }
+        string configFilePath = await CreateConfigFile();
+        Dictionary<string, string> network = await ReadConfigFile(configFilePath);
 
         string ip = "";
         int port = 0;
 
-        using (var streamReader = new StreamReader(pathToEnvFile))
+        ip = network["ip"];
+        try
         {
-            string fileContents = await streamReader.ReadToEndAsync();
-            string[] lines = fileContents.Split('\n');
-
-            foreach (string line in lines)
-            {
-                string[] keyValue = line.Split('=');
-                if (keyValue.Length == 2)
-                {
-                    string key_env = keyValue[0].Trim();
-                    string value = keyValue[1].Trim();
-
-                    switch (key_env)
-                    {
-                        case "IP":
-                            ip = value;
-                            break;
-                        case "PORT":
-                            port = int.Parse(value);
-                            break;
-                    }
-                }
-            }
+            port = int.Parse(network["port"]);
+        }
+        catch
+        {
+            Console.WriteLine($"Используемый порт ({network["port"]}) имеет не верный формат, пожалуйста изменить его в файле config.env и перезапустите программу");
         }
 
-         if (ip == "")
+
+        if (ip == "")
         {
             Console.Write("IP пуст. Введите IP сервера. Вы можете вписать IP сервера в созданный файл config.env, чтобы больше не видеть это сообщение:  ");
-            ip = Console.ReadLine();            
+            ip = Console.ReadLine();
         }
 
         // Сохраняем время
@@ -158,10 +71,10 @@ class Program
         {
             { "os", os },
             { "IsWindowsUpdateNeeded", isWindowsUpdateNeeded },
-            { "driveInfo", driveInfo },
+            { "driveInfo", drivers },
             { "name", machineName },
             { "time", time},
-            { "programNames", displayName },
+            { "programNames", applicationsList },
             { "eventsSystem", eventsSystem },
             { "eventsApplication", eventsApplication }
         };
@@ -179,42 +92,158 @@ class Program
             TcpClient client = new TcpClient(ip, port); // подключаемся к серверу и создаём клиента 
                                                         // Здесь данные должны отправляться на сервер, если в json-файле для временного хранения данных есть данные, данные из json-файла так же отправятся, а сам json-файл - очистится.
             StreamWriter writer = new StreamWriter(client.GetStream());
+            await CreateDataJsonFile(data);
+            // Вот здесь с временного json хранилища данные будут отправляться 
+            await SendDataToServer(writer, client, options);
+        }
+        catch
+        {
+            // сохраняем данные, которые не смогли отправиться из-за неработающего сервера, до следующего запуска программы с работающим сервером         
+            await CreateDataJsonFile(data);
+        }
+    }
 
-            string json_from_jsonFile = "";
-            string dataJson = "";
-            if (File.Exists("data.json"))
+    static bool IsWindowsUpdateNeeded()
+    {
+        UpdateSession updateSession = new UpdateSession();
+        IUpdateSearcher updateSearcher = updateSession.CreateUpdateSearcher();
+        ISearchResult searchResult = updateSearcher.Search("IsInstalled=0");
+
+        return searchResult.Updates.Count > 0;
+    }
+
+    private static async Task<string> GetApplicationsList()
+    {
+        string applicationsList = "";
+        RegistryKey key;
+        int i = 0;
+        int b = 0;
+        key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+        foreach (String keyName in key.GetSubKeyNames())
+        {
+            RegistryKey subkey = key.OpenSubKey(keyName);
+
+            if (subkey.GetValueNames().Contains("DisplayName"))
+                applicationsList += subkey.GetValue("DisplayName") as string + "\n";
+        }
+        return applicationsList;
+    }
+
+    private static async Task<string> GetDriversList()
+    {
+        string driversList = "";
+        foreach (DriveInfo drive in DriveInfo.GetDrives())
+        {
+            if (drive.IsReady)
             {
-                if (await File.ReadAllTextAsync("data.json") != "")
+                double AvailableFreeSpace_Gb = Convert.ToDouble(((drive.AvailableFreeSpace / Convert.ToDouble(1024)) / Convert.ToDouble(1024)) / Convert.ToDouble(1024));
+                AvailableFreeSpace_Gb = Math.Round(AvailableFreeSpace_Gb, 2);
+                driversList += $"Диск {drive.Name}: Свободное место - {AvailableFreeSpace_Gb} Gb\n";
+            }
+        }
+        return driversList;
+    }
+
+    private static async Task<string> GetEventLog(string logType)
+    {
+        EventLog eventLog = new EventLog(logType);
+        // Создаем переменные для хранения нужных нам событий
+        string events = "";
+
+        foreach (EventLogEntry entry in eventLog.Entries)
+        {
+            // Поучаем только ошибки и критические ошибки
+            if (entry.EntryType == EventLogEntryType.Error || entry.EntryType == EventLogEntryType.FailureAudit)
+            {
+                events += entry.EventID.ToString() + "\n" + entry.Source + "\n" + entry.TimeGenerated.ToString() + "\n" + entry.Message + "\n\n";
+            }
+        }
+        return events;
+    }
+
+    private async static Task<string> CreateConfigFile()
+    {
+        string path = Directory.GetCurrentDirectory();
+        string pathToEnvFile = Path.Combine(path, "config.env");
+        if (!File.Exists(pathToEnvFile))
+        {
+            await File.WriteAllTextAsync(pathToEnvFile, $"IP=\r\nPORT=3333");
+        }
+        return pathToEnvFile;
+    }
+
+    private async static Task<Dictionary<string, string>> ReadConfigFile(string configFilePath)
+    {
+        using (var streamReader = new StreamReader(configFilePath))
+        {
+            string fileContents = await streamReader.ReadToEndAsync();
+            string[] lines = fileContents.Split('\n');
+            Dictionary<string, string> network = new Dictionary<string, string>(){
+                { "ip", "-" },
+                {"port","-" }
+            };
+
+            foreach (string line in lines)
+            {
+                string[] keyValue = line.Split('=');
+                if (keyValue.Length == 2)
                 {
-                    dataJson = "," + File.ReadAllText("data.json");
-                    dataJson = dataJson.Remove(1, 1);
-                    json_from_jsonFile = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + dataJson;
-                    await File.WriteAllTextAsync("data.json", json_from_jsonFile);
+                    string key_env = keyValue[0].Trim();
+                    string value = keyValue[1].Trim();
+
+                    switch (key_env)
+                    {
+                        case "IP":
+                            network["ip"] = value;
+                            break;
+                        case "PORT":
+                            network["port"] = value;
+                            break;
+                    }
                 }
-                else
-                {
-                    dataJson = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
-                    await File.WriteAllTextAsync("data.json", dataJson);
-                }
+
+            }
+            return network;
+        }
+    }
+
+    private async static Task CreateDataJsonFile(Dictionary<string, string> data)
+    {
+        string json_from_jsonFile = "";
+        string dataJson = "";
+        if (File.Exists("data.json"))
+        {
+            if (await File.ReadAllTextAsync("data.json") != "")
+            {
+                dataJson = "," + File.ReadAllText("data.json");
+                dataJson = dataJson.Remove(1, 1);
+                json_from_jsonFile = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + dataJson;
+                await File.WriteAllTextAsync("data.json", json_from_jsonFile);
             }
             else
             {
-                json_from_jsonFile = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
-                await File.WriteAllTextAsync("data.json", json_from_jsonFile);
+                dataJson = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
+                await File.WriteAllTextAsync("data.json", dataJson);
             }
+        }
+        else
+        {
+            json_from_jsonFile = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
+            await File.WriteAllTextAsync("data.json", json_from_jsonFile);
+        }
+    }
 
+    private async static Task SendDataToServer(StreamWriter writer, TcpClient client, JsonSerializerOptions options)
+    {
+        // Вот здесь с временного json хранилища данные будут отправляться 
+        string json = await File.ReadAllTextAsync("data.json");
+        List<Data_being_sent> json_objects = JsonConvert.DeserializeObject<List<Data_being_sent>>(json);
 
-
-            // Вот здесь с временного json хранилища данные будут отправляться 
-
-            string json = await File.ReadAllTextAsync("data.json");
-            List<Data_being_sent> json_objects = JsonConvert.DeserializeObject<List<Data_being_sent>>(json);
-
-            Dictionary<string, string> data_json = null;
-            await writer.WriteLineAsync("[");
-            foreach (var item in json_objects)
-            {
-                data_json = new Dictionary<string, string>()
+        Dictionary<string, string> data_json = null;
+        await writer.WriteLineAsync("[");
+        foreach (var item in json_objects)
+        {
+            data_json = new Dictionary<string, string>()
                     {
                         { "os", item.os },
                         { "IsWindowsUpdateNeeded", item.isWindowsUpdateNeeded },
@@ -225,47 +254,14 @@ class Program
                         { "eventsSystem", item.eventsSystem },
                         { "eventsApplication", item.eventsApplication}
                     };
-                await writer.WriteLineAsync(System.Text.Json.JsonSerializer.Serialize(data_json, options));
-                await writer.WriteLineAsync(",");
-            }
-            await writer.WriteLineAsync("]");
-            await File.WriteAllTextAsync("data.json", "");
-            //Здесь уже отправляются данные текущей сессии
-            writer.Flush();
-            client.Close();
+            await writer.WriteLineAsync(System.Text.Json.JsonSerializer.Serialize(data_json, options));
+            await writer.WriteLineAsync(",");
         }
-        catch
-        {
-            // сохраняем данные, которые не смогли отправиться из-за неработающего сервера, до следующего запуска программы с работающим сервером 
-
-  
-            
-            string json = "";
-            string datajson = "";
-            if (File.Exists("data.json"))
-            {
-                if (await File.ReadAllTextAsync("data.json") != "")
-                {
-                    datajson = "," + await File.ReadAllTextAsync("data.json");
-                    datajson = datajson.Remove(1, 1);
-                    json = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + datajson;
-                    await File.WriteAllTextAsync("data.json", json);
-                }
-                else
-                {
-                    json = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
-                    await File.WriteAllTextAsync("data.json", json);
-                }
-            }
-            else
-            {
-                json = "[\n" + JsonConvert.SerializeObject(data, Formatting.Indented) + "\n]";
-                await File.WriteAllTextAsync("data.json", json);
-            }
-            Console.WriteLine("программа не смогла подключиться к серверу");
-
-        }
-
+        await writer.WriteLineAsync("]");
+        await File.WriteAllTextAsync("data.json", "");
+        //Здесь уже отправляются данные текущей сессии
+        writer.Flush();
+        client.Close();
     }
 }
 
